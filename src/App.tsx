b8df86e3b68cafd3a347a33c32a4081e60bfc0d9
hassaNHatem <hassan.hatem.44@gmail.com>
2022-06-28 15:40:28 +0200

    fetched data from the back end

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { useEffect, useState } from "react";
 import "./App.css";
 import Header from "./Header";
@@ -7,10 +8,22 @@ import Series from "./Series";
 
 function App() {
   const [currentTab, setCurrentTab] = useState("movie");
-  const [data, getData] = useState([]);
-  const [searchTerm, setSearchTerm] = useState<String>();
-  const fetchData = async (searchword: string) => {};
-  useEffect(() => {});
+  const [data, setData] = useState([]);
+  const [searchTerm, setSearchTerm] = useState<string>("avengers");
+  const APIKEY = "5b9bd435";
+  const fetchData = async (searchword: string) => {
+    axios
+      .get(
+        `https://www.omdbapi.com/?s=${searchword}&page=1&apiKey=${APIKEY}&type=${currentTab}`
+      )
+      .then((res) => {
+        setData(res.data.Search);
+      });
+  };
+
+  useEffect(() => {
+    fetchData(searchTerm);
+  }, []);
   return (
     <div className="App">
       <Header></Header>
