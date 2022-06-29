7417e9ade4b35350d92f247ac17d6222daf8e9e6
hassaNHatem <hassan.hatem.44@gmail.com>
2022-06-29 15:12:51 +0200

    initial functionality of recent tab

--- src/Recent.tsx
+++ src/Recent.tsx
@@ -1,7 +1,22 @@
 import "./App.css";
-
+import { MdArrowBackIosNew } from "react-icons/md";
+import { useState } from "react";
 function Recent() {
-  return <div className="">Recent</div>;
+  const [display, setDisplay] = useState(true);
+  return (
+    <div className="recent">
+      <div className="recent-header">
+        <h2>Recently Viewd Shows</h2>
+        <MdArrowBackIosNew
+          className={display ? "arrow" : "reverse-arrow"}
+          onClick={() => {
+            setDisplay(!display);
+          }}
+        />
+      </div>
+      {display && <div className="slider">some slider data</div>}
+    </div>
+  );
 }
 
 export default Recent;
