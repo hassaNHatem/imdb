dd2cf9cfca8d7604b87dd2a32a2163682e4df590
hassaNHatem <hassan.hatem.44@gmail.com>
2022-07-03 14:46:14 +0200

    adjusted styling issiues

--- src/Movies.tsx
+++ src/Movies.tsx
@@ -67,6 +67,7 @@ function Movies({
                 style={{
                   position: "absolute",
                   top: distance,
+                  background: `linear-gradient(to bottom, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.5)) , url(${el.Poster})top center / cover no-repeat`,
                 }}
               >
                 <h2 className="title">{movieDetails.Title}</h2>
